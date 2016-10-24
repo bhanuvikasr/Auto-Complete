d2c502abc0f61a49138726f277b3aaee5c94501f
Dion <dion.chung@brightscope.com>
2016-10-23 20:04:36 -0700

    adding benchhash

--- benchdict.cpp
+++ benchdict.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <sstream>
-#include<algorithm>
-#include<set>
-#include<cstdlib>
+#include <algorithm>
+#include <set>
+#include <cstdlib>
 #include "util.h"
 #include "DictionaryTrie.h"
 #include "DictionaryBST.h"
@@ -15,47 +15,55 @@ using std::istream;
 using std::istringstream;
 
 int main(int argc, char**argv){
-	
+
 	int num_iterations = atoi(argv[3]);
 	int min_size = atoi(argv[1]);
-	int step_size =atoi(argv[2]);
+	int step_size = atoi(argv[2]);
 	std::vector<std::pair<int, long long>> results_bst;
 	std::vector<std::pair<int, long long>> results_ht;
 	std::vector<std::pair<int, long long>> results_dt;
-	
+
 	for (int count=0; count < argc; ++count)
         std::cout << count << " " << argv[count] << '\n';
-	
-	//
-	//cout << num_iterations << min_size << step_size;
-	
+
+	// get lines in file
+	ifstream f;
+	string line;
+	int line_count;
+	f.open(argv[4]);
+	for (line_count = 0; getline(f, line); ++line_count);
+	cout << "Line count " << line_count << endl;
+
 	for (int i =0;i<num_iterations;i++){
-		cout << i << " Num of it " << num_iterations << endl;
+		cout << "Iteration " << i+1 << " of " << num_iterations << endl;
 		// reset the if stream
 		ifstream in;
 		Utils u;
 		DictionaryBST d_bst;
 		DictionaryHashtable d_ht;
 		DictionaryTrie dt;
-		
+
 		in.open(argv[4]);
-		
+
 		int size = min_size +i*step_size;
-		//cout << size;
-		
-		u.load_dict(d_bst, in,size);
-		
-		u.load_dict(d_ht, in,size);
-		
-		u.load_dict(dt, in,size);
-		
+
+		int insert_count = size;
+		if (line_count<size) {
+			cout << "WARNING: More words than in file" << endl;
+			insert_count = line_count;
+		}
+
+		u.load_dict(d_bst, in, size);
+
+		u.load_dict(d_ht, in, size);
+
+		u.load_dict(dt, in, size);
+
 		string junk;
 		string data = "";
 		string temp_word = "";
 		string word = "";
-		vector<string> word_string; 
-		//unsigned int i = 0;
-		//bool tt;
+		vector<string> word_string;
 		vector<std::string> hundred_words;
 		int j = 0;
 		while(getline(in, data) && j < 100)
@@ -77,25 +85,23 @@ int main(int argc, char**argv){
 				if(k > 0) word = word + " ";
 				word = word + word_string[k];
 			}
-			
+
 			hundred_words.push_back(word);
 			word_string.clear();
 			j++;
 		}
-		
-		cout << hundred_words.size() << endl ;
-		long long rv =0;
-		int iter = 50000;
-		for(int i =0; i<iter;i++){
+
+		long long rv = 0;
+		int iter = 50;
+		for(int i=0; i<iter; i++){
 			Timer t;
 			t.begin_timer();
 			for(int k=0; k < hundred_words.size(); k++) {
 				d_bst.find(hundred_words[k]);
 			}
-			//cout << "This is k " << k << endl;
 			 rv += t.end_timer();
 		}
-		results_bst.push_back(std::make_pair(size, rv/iter));
+		results_bst.push_back(std::make_pair(insert_count, rv/iter));
 		long long rv2 =0;
 		for(int i =0; i<iter;i++){
 			Timer t2;
@@ -103,10 +109,10 @@ int main(int argc, char**argv){
 			for(int k=0; k < hundred_words.size(); k++) {
 				d_ht.find(hundred_words[k]);
 			}
-			
+
 			rv2 += t2.end_timer();
 		}
-		results_ht.push_back(std::make_pair(size, rv2/iter));		
+		results_ht.push_back(std::make_pair(insert_count, rv2/iter));
 
 		long long rv3 =0;
 		for(int i =0; i<iter;i++){
@@ -117,26 +123,21 @@ int main(int argc, char**argv){
 			}
 			rv3 += t3.end_timer();
 		}
-		results_dt.push_back(std::make_pair(size, rv3/iter));	
-			
-	
-	
+		results_dt.push_back(std::make_pair(insert_count, rv3/iter));
 	}
 
-cout << "DictionaryBST" << endl;
-		//cout << results_bst.size();
-		for (std::vector<std::pair<int, long long>>::iterator it = results_bst.begin() ; it != results_bst.end(); ++it){
-			std::cout << it->first<< ' ' << it->second<< endl;
-		}
-		
-		cout << "DictionaryHashtable" << endl;
-		for (std::vector<std::pair<int, long long>>::iterator it = results_ht.begin() ; it != results_ht.end(); ++it){
-			std::cout << it->first<< ' ' << it->second<< endl;
-		}
-		
-		cout << "DictionaryTrie" << endl;	
-		
-		for (std::vector<std::pair<int, long long>>::iterator it = results_dt.begin() ; it != results_dt.end(); ++it){
-			std::cout << it->first<< ' ' << it->second<< endl;
-		}
+	cout << "DictionaryBST" << endl;
+	for (std::vector<std::pair<int, long long>>::iterator it = results_bst.begin() ; it != results_bst.end(); ++it){
+		cout << it->first<< ' ' << it->second<< endl;
+	}
+
+	cout << "DictionaryHashtable" << endl;
+	for (std::vector<std::pair<int, long long>>::iterator it = results_ht.begin() ; it != results_ht.end(); ++it){
+		cout << it->first<< ' ' << it->second<< endl;
+	}
+
+	cout << "DictionaryTrie" << endl;
+	for (std::vector<std::pair<int, long long>>::iterator it = results_dt.begin() ; it != results_dt.end(); ++it){
+		cout << it->first<< ' ' << it->second<< endl;
+	}
 }
---
+++ benchhash.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <set>
+#include <cstdlib>
+#include <vector>
+#include <array>
+#include <cmath>
+#include "util.h"
+#include "DictionaryTrie.h"
+#include "DictionaryBST.h"
+#include "DictionaryHashtable.h"
+#define LETTERS 26
+using namespace std;
+using std::istream;
+using std::istringstream;
+
+/*
+hash function based on folding
+http://research.cs.vt.edu/AVresearch/hashing/strings.php
+*/
+long sfold(string s, unsigned int m_size) {
+  int intLength = s.length() / 4;
+  long sum = 0;
+  for (int j = 0; j < intLength; j++) {
+    string subs = s.substr(j * 4, (j * 4) + 4);
+    char c[subs.length()+1];
+    strcpy(c, subs.c_str());
+    long mult = 1;
+    for (int k = 0; k < sizeof(c)-1; k++) {
+      sum += c[k] * mult;
+      mult *= 256;
+    }
+  }
+
+  string subs = s.substr(intLength * 4);
+  char c[subs.length()+1];
+  strcpy(c, subs.c_str());
+  long mult = 1;
+  for (int k = 0; k < sizeof(c)-1; k++) {
+    sum += c[k] * mult;
+    mult *= 256;
+  }
+
+  cout << s << " has sum  " << sum << endl;
+  return(abs(sum) % m_size);
+}
+
+/*
+djb2 hash function
+http://www.cse.yorku.ca/~oz/hash.html
+*/
+long djb2_hash(string s, unsigned int m_size) {
+
+  unsigned long hash = 5381;
+  char c[s.length()+1];
+  strcpy(c, s.c_str());
+
+  for (int k = 0; k < sizeof(c)-1; k++) {
+      hash = ((hash << 5) + hash) + (int)c[k]; /* hash * 33 + c */
+  }
+
+  cout << s << " has hash  " << hash << endl;
+  return hash % m_size;
+}
+
+
+int main(int argc, char**argv){
+  string dictfile = argv[1];
+  unsigned int num_words = atoi(argv[2]);
+  unsigned int m_size = 2 * num_words;
+  vector<set<string>> hashtable1;
+  hashtable1.resize(m_size);
+  vector<int> results_ht1;
+  float total_steps1 = 0;
+  int worst_set1 = 0;
+  vector<set<string>> hashtable2;
+  hashtable2.resize(m_size);
+  vector<int> results_ht2;
+  float total_steps2 = 0;
+  int worst_set2 = 0;
+
+  ifstream in;
+  in.open(dictfile);
+  string junk;
+  string data = "";
+  string temp_word = "";
+  string word = "";
+  vector<string> word_string;
+  int j = 0;
+  while(getline(in, data) && j < num_words)
+  {
+    if(in.eof()) break;
+    temp_word = "";
+    word = "";
+    data = data + " .";
+    istringstream iss(data);
+    iss >> junk;
+    while(1)
+    {
+      iss >> temp_word;
+    if(temp_word == ".") break;
+    if(temp_word.length() > 0) word_string.push_back(temp_word);
+    }
+    for(int k = 0; k < word_string.size(); k++)
+    {
+      if(k > 0) word = word + " ";
+      word = word + word_string[k];
+    }
+
+    hashtable1[sfold(word, m_size)].insert(word);
+    hashtable2[djb2_hash(word, m_size)].insert(word);
+    word_string.clear();
+    j++;
+  }
+
+  // calculate statistics
+  for (int k=0; k<hashtable1.size(); k++) {
+    // cout << "Row " << k << endl;
+    // for (set<string>::iterator it=hashtable1[k].begin(); it!=hashtable1[k].end(); ++it) {
+    //   cout << ' ' << *it;
+    // }
+    // cout << endl;
+    if (results_ht1.size()<=hashtable1[k].size()) {
+      worst_set1 = hashtable1[k].size();
+      results_ht1.resize(hashtable1[k].size()+1);
+    }
+    int steps = 0;
+    for (int j=0; j<hashtable1[k].size()+1; j++) steps += j;
+    total_steps1 += steps;
+    results_ht1[hashtable1[k].size()]++;
+  }
+
+  // print statistics
+  cout << "Printing the statistics for Fold Hash Function with hash table size "
+    << m_size << endl;
+  cout << "#hits\t#slots receiving the #hits" << endl;
+  for (int j=0; j<results_ht1.size(); j++) {
+    cout << j << "\t" << results_ht1[j] << endl;
+  }
+  cout << "The average number of steps for a successful search for hash function1 would be "
+    << total_steps1/num_words << endl;
+  cout << "The worst case steps that would be needed to find a word is "
+  << worst_set1 << endl;
+
+  // calculate statistics
+  for (int k=0; k<hashtable2.size(); k++) {
+    // cout << "Row " << k << endl;
+    // for (set<string>::iterator it=hashtable2[k].begin(); it!=hashtable2[k].end(); ++it) {
+    //   cout << ' ' << *it;
+    // }
+    // cout << endl;
+    if (results_ht2.size()<=hashtable2[k].size()) {
+      worst_set2 = hashtable2[k].size();
+      results_ht2.resize(hashtable2[k].size()+1);
+    }
+    int steps = 0;
+    for (int j=0; j<hashtable2[k].size()+1; j++) steps += j;
+    total_steps2 += steps;
+    results_ht2[hashtable2[k].size()]++;
+  }
+
+  // print statistics
+  cout << "Printing the statistics for djb Hash Function with hash table size "
+    << m_size << endl;
+  cout << "#hits\t#slots receiving the #hits" << endl;
+  for (int j=0; j<results_ht2.size(); j++) {
+    cout << j << "\t" << results_ht2[j] << endl;
+  }
+  cout << "The average number of steps for a successful search for hash function2 would be "
+    << total_steps2/num_words << endl;
+  cout << "The worst case steps that would be needed to find a word is "
+    << worst_set2 << endl;
+
+  sfold("aaa", 1000);
+  sfold("aab", 1000);
+  sfold("abcde", 1000);
+  djb2_hash("aaa", 1000);
+  djb2_hash("aab", 1000);
+  djb2_hash("abcde", 1000);
+}
